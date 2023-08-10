82fe1d724b5a02238fcdabdc80e9d38ab061896f
anhtuantu-vnu <18021361@vnu.edu.vn>
2023-08-10 14:32:06 +0700

    fix code review

--- database/migrations/2023_08_10_070850_create_tbl_role.php
+++ database/migrations/2023_08_10_070850_create_tbl_role.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tbl_role', function (Blueprint $table) {
+        Schema::create('role', function (Blueprint $table) {
             $table->id();
             $table->string('uuid')->primary();
             $table->string('description');
